308ed212040b548b00d54d5c4890424e93685512
ldyong <ldyong911@gmail.com>
2019-03-05 14:04:41 +0900

    사용자 등록 및 수정 폼 마이그레이션 및 테스트 코드 작성

--- src/main/java/kr/or/ddit/user/controller/UserController.java
+++ src/main/java/kr/or/ddit/user/controller/UserController.java
@@ -3,14 +3,17 @@ package kr.or.ddit.user.controller;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.List;
 import java.util.Map;
+import java.util.UUID;
 
 import javax.annotation.Resource;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -19,7 +22,10 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.multipart.MultipartFile;
 
+import kr.or.ddit.encrypt.kisa.sha256.KISA_SHA256;
 import kr.or.ddit.user.model.UserVo;
 import kr.or.ddit.user.service.IUserService;
 import kr.or.ddit.util.model.PageVo;
@@ -125,5 +131,116 @@ public class UserController {
 		
 		sos.close();
 		fis.close();
+	}
+	
+	/**
+	 * Method : userForm
+	 * 작성자 : pc11
+	 * 변경이력 :
+	 * @return
+	 * Method 설명 : 사용자 등록폼 요청
+	 */
+	@RequestMapping(path="/userForm", method=RequestMethod.GET)
+	public String userForm(){
+		return "user/userForm";
+	}
+	
+	@RequestMapping(path="/userForm", method=RequestMethod.POST)
+	public String userForm(UserVo userVo,
+						@RequestPart("profile")MultipartFile profile,
+						HttpSession session,
+						Model model) throws Exception{
+		
+		UserVo duplicateUserVo =  userService.selectUser(userVo.getUserId());
+		
+		//중복체크 통과(신규등록)
+		if(duplicateUserVo == null){
+			//사용자 프로파일을 업로드 한경우
+			String filename = "";
+			String realFilename = "";
+			if(profile.getSize() > 0){
+				filename = profile.getOriginalFilename();
+				realFilename = "d:\\picture\\" + UUID.randomUUID().toString();
+				
+				profile.transferTo(new File(realFilename));
+				
+				userVo.setFilename(filename);
+				userVo.setRealFilename(realFilename);
+			}
+			
+			int insertCnt = 0;
+			try {
+				insertCnt = userService.insertUser(userVo);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			
+			//정상입력(성공)
+			if(insertCnt == 1){
+				session.setAttribute("msg", "정상 등록 되었습니다.");
+				return "redirect:/user/userPagingList";  //contextPath 작업 필요
+			}else{
+				return "user/userForm"; //검증 필요
+			}
+		}
+		//중복체크 실패
+		else{
+			model.addAttribute("msg", "등록에 실패 했습니다.");
+			return "user/userForm";
+		}
+	}
+	
+	
+	/**
+	 * Method : userModifyForm
+	 * 작성자 : pc11
+	 * 변경이력 :
+	 * @param userId
+	 * @param model
+	 * @return
+	 * Method 설명 : 사용자 수정폼 요청
+	 */
+	@RequestMapping(path="/userModifyForm", method=RequestMethod.GET)
+	public String userModifyForm(@RequestParam("userId")String userId, Model model){
+		UserVo userVo = userService.selectUser(userId);
+		model.addAttribute("userVo", userVo);
+		
+		return "user/userModifyForm";
+	}
+	
+	@RequestMapping(path="/userModifyForm", method=RequestMethod.POST)
+	public String userModifyForm(UserVo userVo,
+								@RequestPart("profile")MultipartFile profile) throws Exception{
+//		request.setCharacterEncoding("UTF-8");
+		
+		logger.debug("userVo : {}", userVo);
+		
+		//기존 사진 가져오기
+		String filename = "";
+		String realFilename = "";
+		
+		//사진 수정
+		if(profile.getSize() > 0){
+			filename = profile.getOriginalFilename();
+			realFilename = "d:\\picture\\" + UUID.randomUUID().toString();
+			
+			profile.transferTo(new File(realFilename));
+		}
+		userVo.setFilename(filename);
+		userVo.setRealFilename(realFilename);
+		
+		//사용자 수정
+		userVo.setPass(KISA_SHA256.encrypt(userVo.getPass())); //패스워드 암호화
+		int updateCnt = userService.updateUser(userVo);
+		
+		//정상 수정(성공)
+		if(updateCnt == 1){
+//			response.sendRedirect(request.getContextPath() + "/user?userId=" + userId);
+			return "redirect:/user/user?userId=" + userVo.getUserId();
+		}
+		//정상 수정(실패)
+		else{
+			return "user/userModifyForm";
+		}
 	}
 }
--- src/test/java/kr/or/ddit/user/controller/UserControllerTest.java
+++ src/test/java/kr/or/ddit/user/controller/UserControllerTest.java
@@ -1,19 +1,47 @@
 package kr.or.ddit.user.controller;
 
 import static org.junit.Assert.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.fileUpload;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.util.List;
 
+import javax.annotation.Resource;
+import javax.servlet.http.HttpSession;
+
+import org.junit.Before;
 import org.junit.Test;
+import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.web.servlet.ModelAndView;
 
 import kr.or.ddit.test.WebTestConfig;
 import kr.or.ddit.user.model.UserVo;
+import kr.or.ddit.user.service.IUserService;
 
 public class UserControllerTest extends WebTestConfig{
+	
+	private static final String USER_INSERT_TEST_ID = "realTest";
+	
+	@Resource(name="userService")
+	private IUserService userService;
+	
+	@Before
+	public void initData(){
+		userService.deleteUser(USER_INSERT_TEST_ID);
+	}
 
+	/**
+	 * Method : testUserAllList
+	 * 작성자 : pc11
+	 * 변경이력 :
+	 * @throws Exception
+	 * Method 설명 : 사용자 전체 조회 테스트
+	 */
 	@Test
 	public void testUserAllList() throws Exception {
 		/***Given***/
@@ -31,6 +59,13 @@ public class UserControllerTest extends WebTestConfig{
 		assertTrue(userList.size() > 100);
 	}
 	
+	/**
+	 * Method : testUserPagingList
+	 * 작성자 : pc11
+	 * 변경이력 :
+	 * @throws Exception
+	 * Method 설명 : 사용자 페이징 리스트 조회 테스트
+	 */
 	@Test
 	public void testUserPagingList() throws Exception{
 		/***Given***/
@@ -51,6 +86,13 @@ public class UserControllerTest extends WebTestConfig{
 		assertEquals(10, pageSize);
 	}
 	
+	/**
+	 * Method : testUser
+	 * 작성자 : pc11
+	 * 변경이력 :
+	 * @throws Exception
+	 * Method 설명 : 사용자 조회 테스트
+	 */
 	@Test
 	public void testUser() throws Exception{
 		/***Given***/
@@ -64,6 +106,173 @@ public class UserControllerTest extends WebTestConfig{
 		/***Then***/
 		assertEquals("user/user", viewName);
 		assertEquals("brown", userVo.getUserId());
+	}
+	
+	/**
+	 * Method : testUserForm
+	 * 작성자 : pc11
+	 * 변경이력 :
+	 * @throws Exception
+	 * Method 설명 : 사용자 등록 요청 뷰 테스트
+	 */
+	@Test
+	public void testUserForm() throws Exception{
+		/***Given***/
+		
+		/***When***/
+		MvcResult mvcResult = mockMvc.perform(get("/user/userForm")).andReturn();
+		ModelAndView mav = mvcResult.getModelAndView();
+		String viewName = mav.getViewName();
+		
+		/***Then***/
+		assertEquals("user/userForm", viewName);
+	}
+	
+	/**
+	 * Method : testUserForm_post_success
+	 * 작성자 : pc11
+	 * 변경이력 :
+	 * Method 설명 : 사용자 등록 요청 테스트
+	 * @throws Exception 
+	 */
+	@Test
+	public void testUserForm_post_success() throws Exception{
+		/***Given***/
+		File profileFile = new File("D:\\레인저스사진\\real.jpg");
+		FileInputStream fis = new FileInputStream(profileFile);
+		MockMultipartFile file = new MockMultipartFile("profile", "real.jpg", "image/jpg", fis);
+		MvcResult mvcResult = 
+				mockMvc.perform(fileUpload("/user/userForm").file(file)
+															.param("userId", USER_INSERT_TEST_ID)
+															.param("userNm", USER_INSERT_TEST_ID)
+															.param("alias", "레알")
+															.param("addr1", "대전시 중구 대흥로 76")
+															.param("addr2", "2층 DDIT")
+															.param("zipcode", "34942")
+															.param("pass", USER_INSERT_TEST_ID))
+														.andExpect(view().name("redirect:/user/userPagingList"))
+														.andReturn();
+														
+		
+		/***When***/
+		HttpSession session = mvcResult.getRequest().getSession();
+		
+		/***Then***/
+		assertEquals("정상 등록 되었습니다.", session.getAttribute("msg"));
+	}
+	
+	/**
+	 * Method : testUserForm_post_fail_duplicateUser
+	 * 작성자 : pc11
+	 * 변경이력 :
+	 * @throws Exception
+	 * Method 설명 : 사용자 등록요청(중복 사용자로 인한 등록 실패 케이스) 테스트
+	 */
+	@Test
+	public void testUserForm_post_fail_duplicateUser() throws Exception{
+		/***Given***/
+		File profileFile = new File("D:\\레인저스사진\\real.jpg");
+		FileInputStream fis = new FileInputStream(profileFile);
+		MockMultipartFile file = new MockMultipartFile("profile", "real.jpg", "image/jpg", fis);
+		MvcResult mvcResult = 
+				mockMvc.perform(fileUpload("/user/userForm").file(file)
+															.param("userId", "brown")
+															.param("userNm", "realTest")
+															.param("alias", "레알")
+															.param("addr1", "대전시 중구 대흥로 76")
+															.param("addr2", "2층 DDIT")
+															.param("zipcode", "34942")
+															.param("pass", USER_INSERT_TEST_ID))
+														.andExpect(view().name("user/userForm"))
+														.andReturn();
+		
+		/***When***/
+		ModelAndView mav = mvcResult.getModelAndView();
+		String msg = (String) mav.getModel().get("msg");
+		
+		/***Then***/
+		assertEquals("등록에 실패 했습니다.", msg);
+	}
+	
+	/**
+	 * Method : testUserForm_post_fail_insertError
+	 * 작성자 : pc11
+	 * 변경이력 :
+	 * @throws Exception
+	 * Method 설명 : 사용자 등록 (zipcode 사이즈 sql에러) 테스트
+	 */
+	@Test
+	public void testUserForm_post_fail_insertError() throws Exception{
+		/***Given***/
+		File profileFile = new File("D:\\레인저스사진\\real.jpg");
+		FileInputStream fis = new FileInputStream(profileFile);
+		MockMultipartFile file = new MockMultipartFile("profile", "real.jpg", "image/jpg", fis);
+		MvcResult mvcResult = 
+				mockMvc.perform(fileUpload("/user/userForm").file(file)
+															.param("userId", USER_INSERT_TEST_ID)
+															.param("userNm", USER_INSERT_TEST_ID)
+															.param("alias", "레알")
+															.param("addr1", "대전시 중구 대흥로 76")
+															.param("addr2", "2층 DDIT")
+															.param("zipcode", "3494234942")
+															.param("pass", USER_INSERT_TEST_ID))
+														.andExpect(view().name("user/userForm"))
+														.andReturn();
+														
+		
+		/***When***/
+		
+		/***Then***/
+	}
+	
+	/**
+	 * Method : testUserModifyForm
+	 * 작성자 : pc11
+	 * 변경이력 :
+	 * Method 설명 : 사용자 수정 요청 뷰 테스트
+	 * @throws Exception 
+	 */
+	@Test
+	public void testUserModifyForm() throws Exception{
+		/***Given***/
+		
+		/***When***/
+		MvcResult mvcResult = mockMvc.perform(get("/user/userModifyForm").param("userId", "brown2")).andReturn();
+		ModelAndView mav = mvcResult.getModelAndView();
+		String viewName = mav.getViewName();
+
+		/***Then***/
+		assertEquals("user/userModifyForm", viewName);
+	}
+	
+	/**
+	 * Method : testUserModifyForm_post
+	 * 작성자 : pc11
+	 * 변경이력 :
+	 * Method 설명 : 사용자 수정 요청 테스트
+	 * @throws Exception 
+	 */
+	@Test
+	public void testUserModifyForm_post() throws Exception{
+		/***Given***/
+		File profileFile = new File("D:\\레인저스사진\\real.jpg");
+		FileInputStream fis = new FileInputStream(profileFile);
+		MockMultipartFile file = new MockMultipartFile("profile", "real.jpg", "image/jpg", fis);
+		MvcResult mvcResult = 
+				mockMvc.perform(fileUpload("/user/userModifyForm").file(file)
+															.param("userId", "brown2")
+															.param("userNm", "brown2")
+															.param("alias", "레알")
+															.param("addr1", "대전시 중구 대흥로 76")
+															.param("addr2", "2층 DDIT")
+															.param("zipcode", "34942")
+															.param("pass", "brown2"))
+														.andExpect(view().name("redirect:/user/user?userId=brown2"))
+														.andReturn();
+		
+		/***When***/
+		
+		/***Then***/
 
 	}
 	
